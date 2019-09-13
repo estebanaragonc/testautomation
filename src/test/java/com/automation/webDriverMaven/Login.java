31439753907d1f58c5075df4be8af03fa9aa3f0b
Elvis Aragon <Elvis.X.Aragon.-ND@disney.com>
2019-09-13 16:51:32 -0500

    fail

--- src/test/java/com/automation/webDriverMaven/Login.java
+++ src/test/java/com/automation/webDriverMaven/Login.java
@@ -30,7 +30,7 @@ public class Login {
 		
 		WebElement username, password, submit;
 					
-		username = driver.findElement(By.id("login_field"));
+		username = driver.findElement(By.id("login_fiseld"));
 		username.sendKeys("este es mi usuario");
 		Thread.sleep(3000);
 		password =  driver.findElement(By.name("password"));//
