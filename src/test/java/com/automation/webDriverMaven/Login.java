c06af34d9636d3ee7ca07fe04248fc3441c5afc3
Elvis Aragon <Elvis.X.Aragon.-ND@disney.com>
2019-09-13 13:55:20 -0500

    initial commit

---
+++ src/test/java/com/automation/webDriverMaven/Login.java
@@ -0,0 +1,49 @@
+package com.automation.webDriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Personal\\eclipse\\webDriverMaven\\driver\\chromedriver_win32\\chromedriver.exe");
+		driver =  new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() throws InterruptedException 
+	{
+		
+		driver.get("https://github.com/login");
+		
+		WebElement username, password, submit;
+					
+		username = driver.findElement(By.id("login_field"));
+		username.sendKeys("este es mi usuario");
+		Thread.sleep(3000);
+		password =  driver.findElement(By.name("password"));
+		password.sendKeys("esta es mi constraseña");
+		Thread.sleep(3000);
+		submit = driver.findElement(By.name("commit"));
+		submit.click();
+		
+	}
+	 
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.close();
+		driver.quit();
+	}
+
+}
